6eef4a558c67eab647bd12d64d33938279234585
kushalgulia <kushalgulia896@gmail.com>
2023-03-24 10:23:35 +0530

    refactoring for MVC

---
+++ src/js/controller/controller.js
@@ -0,0 +1,34 @@
+import * as model from '../model/model.js';
+import recipeView from '../views/recipeView.js';
+
+////////////////////////////////////////////////////////////////
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    //render spinner
+    recipeView.renderSpinner();
+    //load recipe
+    await model.loadRecipe(id);
+    //render recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipes)
+);
---
+++ src/js/model/model.js
@@ -0,0 +1,26 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    let { recipe } = data.data; //from the api
+    state.recipe = {
+      id: recipe.id,
+      ingredients: recipe.ingredients,
+      servings: recipe.servings,
+      publisher: recipe.publisher,
+      title: recipe.title,
+      image: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+      source: recipe.source_url,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
